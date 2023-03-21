379fe2d23eff28820836e10fd040f9f5adf88b4f
test <raghadbaniirshaied99@gmail.com>
2023-03-21 14:30:59 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace upload
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string ImageLocation { get; private set; }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "jpg files (*.jpg)|*.jpg| PNG files(*.png)|*.png| AllFiles(*.*)|*.*";
+
+                if(dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                   ImageLocation = dialog.FileName;
+
+                    image1.ImageLocation = ImageLocation;
+                }
+
+            }
+            catch (Exception) {
+                MessageBox.Show("An Error Occured", "Error", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+    }
+}
